ad1f49c389cabe326269c9548344bd514da02e24
caiya928 <caiya928@aliyun.com>
2017-05-13 13:37:16 +0800

    删除多余文件

---
+++ src/components/base/Page.vue
@@ -0,0 +1,15 @@
+<template>
+  <Page :current="current" :page-size="size" :total="total" simple class="page"></Page>  
+</template>
+<script>
+  export default {
+    props: ['total', 'current', 'size']
+  }
+</script>
+
+<style scoped>
+  .page{
+    margin-top: 15px;
+    float: right;
+  }
+</style>
